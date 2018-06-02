990bc8e364865c16978c6340e9ab04c1c4381e6d
Ludovic Marechal <ludovicmarechal.pro@gmail.com>
2018-06-02 14:31:14 +0200

    Migrate webApp

---
+++ truffle.js
@@ -0,0 +1,19 @@
+module.exports = {
+   networks: {
+  development: {
+    host: "192.168.1.12",
+    port: 8545,
+    network_id: "*" // match any network
+  },
+  docker: {
+    host: "192.168.99.100", // docker machine ip
+    port: 8545,
+    network_id: "*",  // match any network
+  },
+  test: {
+    host: "192.168.0.49", // ip for android test
+    port: 8545,
+    network_id: "*",  // match any network
+  }
+}
+};
